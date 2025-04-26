0d98997b02a038fc569649e9cbdffa58113557dd
Jim Shadrick <jshadrick@live.com>
2025-04-26 14:43:42 -0500

    Update templates, styles, and routing for improved layout
    Refactored navigation in `base.html` using nav-links instead of buttons. Updated routing to include `core.urls` and changed the home page template to `index.html` in `HomeView`. Added custom CSS for navigation pills to align with the updated design.

--- project/urls.py
+++ project/urls.py
@@ -4,11 +4,11 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html")),
     path("accounts/", include("allauth.urls")),
     path("accounts/profile/", TemplateView.as_view(template_name="profile.html")),
     path("admin/", admin.site.urls),
     path("i18n/", include("django.conf.urls.i18n")),
+    path("", include("core.urls")),
 ]
 
 if settings.DEBUG:
