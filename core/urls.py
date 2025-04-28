c94e9306231af2a430a67a519ae21bb0eeb8d9bf
Jim Shadrick <jshadrick@live.com>
2025-04-28 15:58:33 -0500

    Add Terms and Conditions and Privacy Policy pages
    Introduced dedicated HTML templates for Terms and Conditions and Privacy Policy. Updated URL patterns, views, and footer links to incorporate these new pages. Refactored template structure for better organization and reusability.

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import HomeView
+
+from .views import HomeView, PrivacyPolicyView, TermsAndConditionsView
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path("privacy-policy/", PrivacyPolicyView.as_view(), name="privacy_policy"),
+    path("terms-and-conditions/", TermsAndConditionsView.as_view(), name="terms_and_conditions"),
 ]
--- core/views.py
+++ core/views.py
@@ -2,4 +2,12 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    template_name = "index.html"
+    template_name = "core/index.html"
+
+
+class PrivacyPolicyView(TemplateView):
+    template_name = "core/privacy_policy.html"
+
+
+class TermsAndConditionsView(TemplateView):
+    template_name = "core/terms_conditions.html"
